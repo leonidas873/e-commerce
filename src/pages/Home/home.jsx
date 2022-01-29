ae71e8b2106a1fd5dfb37704e298e9e60c0da8ff
levanixaburzania1 <levanixaburzania1@gmail.com>
2022-01-29 15:11:50 +0400

    com

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Home = () => {
+    return <HomeStyled>  
+        home
+    </HomeStyled>
+}
+
+const HomeStyled = styled.div`
+    background-color:red;
+`
