0272deb7e1ae3b30c455ae3ea8c7f4cb622bec58
Nika Lukava <Nikolla-L2002@users.noreply.github.com>
2022-02-22 17:14:01 +0400

    implement google auth

--- src/auth/GoogleAuth.jsx
+++ src/auth/GoogleAuth.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import axios  from "axios"
 import GoogleLogin from "react-google-login"
+import styled from 'styled-components';
 
 // UNDA GAESHVAS npm react-google-login axios (AN yarn add DA fetchZEC SHEDIZLEBA GADAKETEBA)
 const GoogleAuth = () => {
@@ -8,8 +9,16 @@ const GoogleAuth = () => {
 
     const onSuccess = async res => {
         try {
+            // const result = await axios.post("http://localhost:3001/auth/google-auth", {
             const result = await axios.post("https://ecommerse--watamasheba.herokuapp.com/auth/google-auth", {
                 token: res?.tokenId,
+            },{
+              withCredentials: true,
+              credentials: 'include',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
             });
             console.log(result.data.user)
             setUser(result.data.user)
@@ -18,24 +27,46 @@ const GoogleAuth = () => {
         }
     };
 
-  return (
-    <div className="h-screen w-screen flex items-center justify-center flex-col">
-      {!user && (
-        <GoogleLogin
-          clientId={`1068850605287-o1pignk020ft6da6c5ijovube2km1k49.apps.googleusercontent.com`}
-          onSuccess={onSuccess}
-        />
-      )}
+    const Button = styled.button`
+      width: 180px;
+      height: 42px;
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      jusfity-content: flex-start;
+      padding: 0 8px;
+      border-radius: 8px;
+      background-color: #fff;
+    `
+    const Avatar = styled.img`
+      width: 28px;
+      height: 28px;
+      border-radius: 50%;
+    `
+    const Name = styled.h1`
+      font-size: 18px;
+      color: gray;
+      margin: 0;
+    `
 
-      {user && (
-        <>
-          <img src={user.avatar} className="rounded-full" />
-          <h1 className="text-xl font-semibold text-center my-5">
-            {user.name}
-          </h1>
-        </>
-      )}
-    </div>
+    return (
+      <div className="h-screen w-screen flex items-center justify-center flex-col">
+        {!user && (
+          <GoogleLogin
+            clientId={`1068850605287-o1pignk020ft6da6c5ijovube2km1k49.apps.googleusercontent.com`}
+            onSuccess={onSuccess}
+          />
+        )}
+
+        {user && (
+          <Button>
+            <Avatar src={user?.picture} />
+            <Name>
+              {user?.name}
+            </Name>
+          </Button>
+        )}
+      </div>
   );
 };
 
